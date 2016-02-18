ba9b6425ceb092d7c63a51b33655e44997d7fce2
pthreat <jpfstange@gmail.com>
2016-02-18 17:40:01 -0300

    Improved and normalized Directories configuration :)

--- class/core/project/config/cli/directories/Helper.class.php
+++ class/core/project/config/cli/directories/Helper.class.php
@@ -3,7 +3,9 @@
 	namespace apf\core\project\config\cli\directories{
 
 		use \apf\iface\config\Nameable;
-
+		use \apf\iface\Log									as	LogInterface;
+		use \apf\core\Cmd;
+		use \apf\core\Directory								as	Dir;
 		use \apf\iface\config\RootDirectory;
 		use \apf\iface\config\module\Directories		as	ModuleDirectories;
 		use \apf\iface\config\sub\Directories			as	SubDirectories;
@@ -12,52 +14,10 @@
 
 		class Helper{
 
-			private static function setDefaults(&$config){
-
-				$mainDir	=	NULL;
-
-				if($config instanceof RootDirectory){
-
-					if($config->getRootDirectory()){
-
-						$mainDir	=	$config->getRootDirectory();
-
-					}elseif($config instanceof Nameable){
-
-						$mainDir		=	new Dir(realpath(getcwd()));
-						$mainDir->addPath($config->getName());
-
-					}
-
-					$config->setRootDirectory($mainDir);
-
-				}
-
-				if($config instanceof ModuleDirectories){
-
-					$modulesDir	=	clone($mainDir);
-					$modulesDir->addPath('modules');
-
-					$config->setModulesDirectory($modulesDir);
-
-				}
-
-				if($config instanceof TemplateDirectories){
-
-					$templatesDir	=	clone($mainDir);
-					$templatesDir->addPath('templates');
-
-				}
-
-				if($config instanceof FragmentDirectories){
-
-					$fragmentsDir	=	clone($mainDir);
-					$fragmentsDir->addPath('fragments');
-					$config->setFragmentsDirectories($fragmentsDir);
-
-				}
-
-			}
+			use \apf\traits\config\cli\RootDirectory;
+			use \apf\traits\config\cli\module\Directories;
+			use \apf\traits\config\cli\template\Directories;
+			use \apf\traits\config\cli\fragment\Directories;
 
 			public static function reset(&$config){
 
@@ -69,7 +29,13 @@
 
 				if($config instanceof ModuleDirectories){
 
-					$config->unsetModulesDirectories();
+					$config->unsetModulesDirectory();
+
+				}
+
+				if($config instanceof SubsDirectories){
+
+					$config->unsetSubsDirectory();
 
 				}
 
@@ -85,19 +51,243 @@
 
 				}
 
-				if($config instanceof SubDirectories){
+			}
 
-					$config->unsetSubsDirectory();
+			public static function defaults(&$config){
+
+				$mainDir		=	new Dir(realpath(getcwd()));
+
+				if($config instanceof RootDirectory){
+
+					if($config->getRootDirectory()){
+
+						$mainDir	=	$config->getRootDirectory();
+
+					}elseif($config instanceof Nameable){
+
+						$mainDir->addPath($config->getName());
+
+					}
+
+				}
+
+				$config->setRootDirectory($mainDir);
+
+				if($config instanceof ModuleDirectories){
+
+					$modulesDir	=	clone($mainDir);
+					$modulesDir->addPath('modules');
+
+					$config->setModulesDirectory($modulesDir);
+
+				}
+
+				if($config instanceof SubsDirectories){
+
+					$subsDir	=	clone($mainDir);
+					$subsDir->addPath('subs');
+
+					$config->setSubsDirectory($subsDir);
+
+				}
+
+				if($config instanceof TemplateDirectories){
+
+					$templatesDir	=	clone($mainDir);
+					$templatesDir->addPath('templates');
+
+					$config->setTemplatesDirectory($templatesDir);
+
+				}
+
+				if($config instanceof FragmentDirectories){
+
+					$fragmentsDir	=	clone($mainDir);
+					$fragmentsDir->addPath('fragments');
+					$config->setFragmentsDirectory($fragmentsDir);
 
 				}
 
 			}
 
-			public static function getMenu(&$object){
+			public static function getMenu(&$config,Array $valuesExcept=Array(),$extraMenus=Array(),$customMenus=Array()){
 
+				$allConfigured	=	$config->hasValuesExcept($valuesExcept);
+
+				$menu				=	Array();
+
+				if($config instanceof RootDirectory){
+
+					$menu['R']	=	Array(
+												'value'	=>	sprintf('Configure root directory (%s)',$config->getRootDirectory()),
+												'color'	=>	$config->getRootDirectory() ? 'light_purple'	:	'light_cyan'
+					);
+
+				}
+
+				if($config instanceof TemplateDirectories){
+
+					$menu['T']	=	Array(
+												'value'	=>	sprintf('Configure templates directory (%s)',$config->getTemplatesDirectory()),
+												'color'	=>	$config->getTemplatesDirectory() ? 'light_purple'	:	'light_cyan'
+					);
+
+				}
+
+				if($config instanceof FragmentDirectories){
+
+					$menu['F']	=	Array(
+												'value'	=>	sprintf('Configure fragments directory (%s)',$config->getFragmentsDirectory()),
+												'color'	=>	$config->getFragmentsDirectory() ? 'light_purple'	:	'light_cyan'
+					);
+
+				}
+
+				if($config instanceof ModuleDirectories){
+
+					$menu['M']	=	Array(
+												'value'	=>	sprintf('Configure modules directory (%s)',$config->getModulesDirectory()),
+												'color'	=>	$config->getModulesDirectory() ? 'light_purple'	:	'light_cyan'
+					);
+
+				}
+
+				if($config instanceof SubDirectories){
+
+					$menu['U']	=	Array(
+												'value'	=>	sprintf('Configure sub (modules) directory (%s)',$config->getSubsDirectory()),
+												'color'	=>	$config->getSubsDirectory() ? 'light_purple'	:	'light_cyan'
+					);
+
+				}
+
+				if($allConfigured && in_array('reset',$extraMenus)){
+
+					$menu['E']	=	Array(
+												'value'	=>	'Reset configuration',
+												'color'	=>	$allConfigured	?	'red'			:	'yellow'
+					);
+
+				}
+
+				if(in_array('defaults',$extraMenus)){
+
+					$menu['L']	=	Array(
+												'value'	=>	'Load defaults',
+												'color'	=>	$allConfigured	?	'yellow'		:	'light_green'
+					);
+
+				}
+
+				if(in_array('save',$extraMenus)){
+
+					$menu['S']	=	Array(
+												'value'	=>	'Save',
+												'color'	=>	$allConfigured	?	'yellow'			:	'light_green'
+					);
+
+				}
+
+				if(in_array('back',$extraMenus)){
+
+					$menu['B']	=	Array(
+												'value'	=>	'Back',
+												'color'	=>	$allConfigured	?	'light_cyan'	:	'yellow'
+					);
+
+				}
+
+				return $menu;
+
+			}
+
+			public static function switchMenuOption($opt,&$config,$allConfigured=FALSE,LogInterface &$log){
+
+				switch(trim(strtolower($opt))){
+
+					case 'r':
+
+						self::configureRootdirectory($config,$log);
+
+					break;
+
+					case 'e':
+
+						if($config->hasValues() && Cmd::yesNo('Are you sure you want to reset the entire configuration?',$log)){
+
+							self::reset($config);
+
+						}
+
+					break;
+
+					case 't':
+
+						self::configureTemplateDirectories($config,$log);
+
+					break;
+
+					case 'f':
+
+						self::configureFragmentDirectories($config,$log);
+
+					break;
+
+					case 'm':
+
+						self::configureModuleDirectories($config,$log);
+
+					break;
+
+					case 'u':
+
+						self::configureSubDirectories($config,$log);
+
+					break;
+
+					case 'b':
+
+						//No values, assume safe "back"
+
+						if(!$config->hasValuesExcept('project')||$allConfigured){
+
+							return FALSE;
+
+						}
+
+						$log->warning("You have unsaved changes in this configuration.");
+
+						if(Cmd::yesNo("Are you sure you want to go back without saving?",$log)){
+
+							return FALSE;
+
+						}
+
+					break;
+
+					case 'l':
+
+						if(!$config->hasValues()||$allConfigured){
+
+							$log->warning("You have configured some directories");
+
+							if(!Cmd::yesNo("Are you sure you want to load the default values?",$log)){
+
+								return;
+
+							}
+
+						}
+						
+						self::defaults($config);
+
+					break;
+
+				}
 
 			}
 
 		}
 
 	}
+
--- class/core/project/directories/config/Cli.class.php
+++ class/core/project/directories/config/Cli.class.php
@@ -3,14 +3,15 @@
 	namespace apf\core\project\directories\config{
 
 		use \apf\core\Project;
-		use \apf\core\project\Directories					as	ProjectDirectories;
-		use \apf\core\project\directories\Config			as	ProjectDirectoriesConfig;
-		use \apf\iface\Log										as	LogInterface;
-		use \apf\core\project\Config							as	ProjectConfig;
+		use \apf\core\project\Directories							as	ProjectDirectories;
+		use \apf\core\project\directories\Config					as	ProjectDirectoriesConfig;
+		use \apf\iface\Log												as	LogInterface;
+		use \apf\core\project\Config									as	ProjectConfig;
+		use \apf\core\project\config\cli\directories\Helper	as	DirectoriesHelper;
 
 		use \apf\core\Cmd;
-		use \apf\core\Directory									as	Dir;
-		use \apf\iface\config\Cli								as	CliConfigInterface;
+		use \apf\core\Directory											as	Dir;
+		use \apf\iface\config\Cli										as	CliConfigInterface;
 
 		class Cli implements CliConfigInterface{
 
@@ -18,46 +19,6 @@
 			use \apf\traits\config\cli\module\Directories;
 			use \apf\traits\config\cli\template\Directories;
 			use \apf\traits\config\cli\fragment\Directories;
-
-			private static function setDefaults(ProjectDirectoriesConfig $config){
-
-				$projectConfig	=	$config->getProject()->getConfig();
-
-				if($config->getRootDirectory()){
-
-					$mainDir	=	$config->getRootDirectory();
-
-				}else{
-
-					$mainDir		=	new Dir(realpath(getcwd()));
-					$mainDir->addPath($projectConfig->getName());
-
-				}
-
-				$modulesDir	=	clone($mainDir);
-				$modulesDir->addPath('modules');
-
-				$templatesDir	=	clone($mainDir);
-				$templatesDir->addPath('templates');
-
-				$fragmentsDir	=	clone($mainDir);
-				$fragmentsDir->addPath('fragments');
-
-				$config->setRootDirectory($mainDir);
-				$config->setModulesDirectory($modulesDir);
-				$config->setTemplatesDirectory($templatesDir);
-				$config->setFragmentsDirectory($fragmentsDir);
-
-			}
-
-			public function resetConfiguration(&$config){
-
-				$config->unsetRootDirectory();
-				$config->unsetModulesDirectory();
-				$config->unsetFragmentsDirectory();
-				$config->unsetTemplatesDirectory();
-
-			}
 
 			/**
 			 * Configure project directories.
@@ -78,102 +39,23 @@
 			public static function configure(&$config=NULL, LogInterface &$log){
 
 				$config	=	new ProjectDirectoriesConfig($config);
+				$extraMenus	=	Array('back','save','defaults','reset');
 
 				do{
 
 					try{
 
-						$allConfigured	=	$config->getRootDirectory()		&& 
-												$config->getFragmentsDirectory() && 
-												$config->getTemplatesDirectory() && 
-												$config->getModulesDirectory();
-
-						$menu				=	Array(
-															'R'	=>	Array(
-																				'value'	=>	sprintf('Configure root directory (%s)',$config->getRootDirectory()),
-																				'color'	=>	$config->getRootDirectory() ? 'light_purple'	:	'light_cyan'
-															),
-															'T'	=>	Array(
-																				'value'	=>	sprintf('Configure templates directory (%s)',$config->getTemplatesDirectory()),
-																				'color'	=>	$config->getTemplatesDirectory() ? 'light_purple'	:	'light_cyan'
-															),
-															'F'	=>	Array(
-																				'value'	=>	sprintf('Configure fragments directory (%s)',$config->getFragmentsDirectory()),
-																				'color'	=>	$config->getFragmentsDirectory() ? 'light_purple'	:	'light_cyan'
-															),
-															'M'	=>	Array(
-																				'value'	=>	sprintf('Configure modules directory (%s)',$config->getModulesDirectory()),
-																				'color'	=>	$config->getModulesDirectory() ? 'light_purple'	:	'light_cyan'
-															),
-															'L'	=>	Array(
-																				'value'	=>	'Load defaults',
-																				'color'	=>	$allConfigured	?	'yellow'			:	'light_green'
-															)
-						);
-
-
-						if($config->hasValuesExcept('project')){
-
-							$menu['E']	=	Array(
-														'value'	=>	'Reset configuration',
-														'color'	=>	$allConfigured	?	'red'				:	'yellow'
-							);
-
-						}
-
-						$menu['S']	=	Array(
-													'value'	=>	'Save',
-													'color'	=>	$allConfigured	?	'light_green'	:	'yellow',
-						);
-
-						$menu['B']	=	Array(
-													'value'	=>	'Back',
-													'color'	=>	$allConfigured	?	'light_cyan'	:	'yellow'
-						);
+						$menu				=	DirectoriesHelper::getMenu($config,$valuesExcept=Array('project'),$extraMenus);
+						$allConfigured	=	$config->hasValuesExcept(Array('project'));
 
 						Cmd::clear();
 
 						$log->debug("[ Configure project directories ]");
-
 						$log->repeat('-',80,'light_purple');
 
 						$opt	=	Cmd::selectWithKeys($menu,'directories>',$log);
 
 						switch(trim(strtolower($opt))){
-
-							case 'r':
-
-								self::configureRootdirectory($config,$log);
-
-							break;
-
-							case 't':
-
-								self::configureTemplateDirectories($config,$log);
-
-							break;
-
-							case 'f':
-
-								self::configureFragmentDirectories($config,$log);
-
-							break;
-
-							case 'm':
-
-								self::configureModuleDirectories($config,$log);
-
-							break;
-
-							case 'e':
-
-								if($config->hasValues() && Cmd::yesNo('Are you sure you want to reset the entire configuration?',$log)){
-
-									self::resetConfiguration($config);
-
-								}
-
-							break;
 
 							case 's':
 
@@ -193,32 +75,9 @@
 
 							break;
 
-							case 'l':
+							default:
 
-								if($allConfigured){
-
-									$log->warning('Everything seems to be configured, are you sure you want to set defaults?');
-									Cmd::yesNo('>',$log);
-
-								}
-
-								self::setDefaults($config);
-
-							break;
-
-							case 'b':
-
-								//No values, assume safe "back"
-
-								if(!$config->hasValues()||$allConfigured){
-
-									return FALSE;
-
-								}
-
-								$log->warning("You have unsaved changes in this configuration.");
-
-								if(Cmd::yesNo("Are you sure you want to go back without saving?",$log)){
+								if(DirectoriesHelper::switchMenuOption($opt,$config,$allConfigured,$log) === FALSE){
 
 									return FALSE;
 
