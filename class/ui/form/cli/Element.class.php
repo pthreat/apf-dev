2185d2dd15828950162ea0024ba551216bc08906
Federico Stange <federico.stange@educacion.local>
2016-03-09 16:45:58 -0300

    Applying new merged configuration pattern

--- class/ui/form/cli/Element.class.php
+++ class/ui/form/cli/Element.class.php
@@ -5,57 +5,9 @@
 		use \apf\console\Ansi;
 		use \apf\ui\form\Element								as	BaseElement;
 		use \apf\ui\form\cli\element\Layout					as	ElementLayout;
-		use \apf\iface\ui\form\cli\element\Promptable	as	PromptableInterface;
 		use \apf\iface\ui\form\element\layout\Container	as	ElementLayoutContainerInterface;
 
-		abstract class Element extends BaseElement implements PromptableInterface{
-
-			use \apf\traits\ui\form\cli\element\Promptable;
-
-			public function __construct($attrName,$description,ElementLayoutContainerInterface $layoutContainer){
-
-				if(!array_key_exists('noval',$layouts)){
-
-					$layouts['noval']	=	new ElementLayout($this,'[name:{"color":"light_cyan"}]> [description]');
-
-				}
-
-				if(!array_key_exists('success',$layouts)){
-
-					$layouts['success']	=	new ElementLayout($this,'[name:{"color":"light_green"}]> [description] [value:{"format":"(%s)"}]');
-
-				}
-
-				if(!array_key_exists('error',$layouts)){
-
-					$layouts['error']	=	new ElementLayout($this,'[name:{"color":"red"}]> [description] [value:{"format":"<%s>"}]');
-
-				}
-
-				parent::__construct($attrName,$description,$layouts);
-
-			}
-
-			public function setValue($value=NULL){
-
-				do{
-
-					parent::setValue($this->getPrompt()->read($value));
-
-					if($this->getValueState()=='error'){
-
-						echo $e->getMessage();
-
-						(new Prompt())
-						->setText('Press enter to continue ...')
-						->read();
-
-					}
-
-
-				}while($this->getValueState()!=='success');
-
-			}
+		abstract class Element extends BaseElement{
 
 		}		
 
